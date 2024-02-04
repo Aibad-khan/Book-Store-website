b858b0177e97958582b474dabc4f7ad8dcf9b8fd
Aibad <143527312+Aibad-khan@users.noreply.github.com>
2024-02-04 14:19:43 +0530

    Add files via upload

---
+++ book store website/project/footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +91-451-1290 </p>
+         <p> <i class="fas fa-phone"></i> +91-2456-33223 </p>
+         <p> <i class="fas fa-envelope"></i> aibadkhan812@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Noida, india - 201109 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> meta </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span> web developer</span> </p>
+
+</section>
